794998b31904721f4f6a87006a5f4bbfc03cde75
Tobias O <wi20092@lehre.dhbw-stuttgart.de>
2021-09-27 19:45:15 +0200

    Weiterer Test

--- Passworteingabe.java
+++ Passworteingabe.java
@@ -5,7 +5,7 @@ public class Passworteingabe {
     static JPasswordField PasswortFeld = new JPasswordField();
     public static void main(String[] args) {
     System.out.println("Test Neu");
-    System.out.println("Weiterer Teeeeest");
+    System.out.println("Weiterer   Test");
 
     }
 }
